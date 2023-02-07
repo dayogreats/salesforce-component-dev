6e25b5b635963dcbb030dafbdbb134f7f22dad48
Dayo Greats <dayogreats@gmail.com>
2023-02-06 22:18:30 -0500

    add bidder component

--- sfd-lwc-dev/force-app/main/default/lwc/bidder/bidder.js
+++ sfd-lwc-dev/force-app/main/default/lwc/bidder/bidder.js
@@ -1,3 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,  track } from 'lwc';
 
-export default class Bidder extends LightningElement {}
+export default class Bidder extends LightningElement {
+    @track 
+    color= "red";
+    name="";
+    tag = 101;
+    amount = 0.0;
+    
+    
+    handleOnChange(e){
+        this.primaryColor= e.target.value
+    }
+
+    tagOnChange(e){
+        this.tag= e.target.value
+    }
+
+    nameOnChange(e){
+        this.name= e.target.value
+    }
+
+    amountOnChange(e){
+        this.amount= e.target.value
+    }
+
+}
